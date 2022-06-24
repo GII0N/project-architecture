de18898305a2d4d3b2d08ff2ceeb628aa03aae9a
LegacyOfTheArdor <95230814+LegacyOfTheArdor@users.noreply.github.com>
2022-06-24 13:39:17 +0200

    menus and bars

---
+++ Assets/Scripts/Menu Scripts/PauseMenu.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+	public GameObject InGameHud;
+	public GameObject Pausemenu;
+	public GameObject ExitMenu;
+	public GameObject SettingsMenu;
+	public GameObject SaveMenu;
+	public float progress = 0;
+	public Slider ProgressSlider;
+
+
+	private void Start()
+	{
+		Time.timeScale = 1f;
+	}
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			if (Time.timeScale == 0f)
+			{
+				Resume();
+			}
+			else
+			{
+				Pause();
+			}
+		}
+
+		UpdateProgress();
+
+	}
+	public void Resume()
+	{
+		InGameHud.SetActive(true);
+		Pausemenu.SetActive(false);
+		Time.timeScale = 1f;
+		Cursor.lockState = CursorLockMode.Locked;
+		
+	}
+
+	void Pause()
+	{
+		InGameHud.SetActive(false);
+		Pausemenu.SetActive(true);
+		Time.timeScale = 0f;
+		Cursor.lockState = CursorLockMode.None;
+		
+	}
+
+	public void ExitGame()
+	{
+		ExitMenu.SetActive(true);
+	}
+
+	public void ConfirmExit()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+	public void CancelExit()
+	{
+		ExitMenu.SetActive(false);
+	}
+
+	public void OpenSettings()
+	{
+		SettingsMenu.SetActive(true);
+	}
+
+	public void OpenSaveMenu()
+	{
+		SaveMenu.SetActive(true);
+	}
+
+	public void UpdateProgress()
+	{
+		progress++;
+		ProgressSlider.value = progress;
+	}
+}
---
+++ Assets/Scripts/Menu Scripts/SaveMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveMenu : MonoBehaviour
+{
+	public GameObject Savemenu;
+	public GameObject SaveGame;
+	public GameObject LoadGame;
+	public GameObject NewGame;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void CloseSave()
+	{
+		Savemenu.SetActive(false);
+	}
+
+	public void OpenSave()
+	{
+		SaveGame.SetActive(true);
+	}
+
+	public void OpenLoad()
+	{
+		LoadGame.SetActive(true);
+	}
+
+	public void OpenNewGame()
+	{
+		NewGame.SetActive(true);
+	}
+}
