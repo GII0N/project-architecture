f25aa0435ba1f372e8de32230eb79c7f03b1109b
Gideon Tigelaar <gidtigelaar@gmail.com>
2022-07-04 16:43:21 +0200

    NPC Menu

--- Assets/Scripts/Player Scripts/PlayerInteraction.cs
+++ Assets/Scripts/Player Scripts/PlayerInteraction.cs
@@ -9,6 +9,7 @@ public class PlayerInteraction : MonoBehaviour
 
     public GameObject npcText;
     public GameObject npcMenu;
+    public GameObject InGameHud;
 
     private void Update()
     {
@@ -19,13 +20,32 @@ public class PlayerInteraction : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.E))
             {
                 npcMenu.SetActive(true);
+                Time.timeScale = 0f;
+                Cursor.lockState = CursorLockMode.None;
                 triggering = false;
+                InGameHud.SetActive(false);
             }
         }
         else
         {
             npcText.SetActive(false);
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+		{
+            npcMenu.SetActive(false);
+            Time.timeScale = 1f;
+            Cursor.lockState = CursorLockMode.Locked;
+            InGameHud.SetActive(true);
+		}
+    }
+
+    public void CloseMenu()
+    {
+        npcMenu.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        InGameHud.SetActive(true);
     }
 
     private void OnTriggerEnter(Collider collider)
