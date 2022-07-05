48f16b588456f2eb4f3a0503a31e6ada09e05f97
Gideon Tigelaar <gidtigelaar@gmail.com>
2022-07-05 11:36:25 +0200

    Changed Alot in the Shop

--- Assets/Scripts/Player Scripts/PickUpObject.cs
+++ Assets/Scripts/Player Scripts/PickUpObject.cs
@@ -19,6 +19,18 @@ public class PickUpObject : MonoBehaviour
     public GameObject Building7;
     public GameObject Building8;
     public GameObject Building9;
+    public GameObject Building10;
+    public GameObject Building11;
+    public GameObject Building12;
+    public GameObject Building13;
+    public GameObject Building14;
+    public GameObject Building15;
+    public GameObject Building16;
+    public GameObject Building17;
+    public GameObject Building18;
+    public GameObject Building19;
+    public GameObject Building20;
+    public GameObject Building21;
 
     public TextMeshProUGUI woodCounter;
     public TextMeshProUGUI woodCounter1;
@@ -29,7 +41,6 @@ public class PickUpObject : MonoBehaviour
 
     private void OnTriggerEnter(Collider collider)
     {
-        //prevent it from being below zero when buying stuff
         if (collider.transform.tag == "Wood")
         {
             wood++;
@@ -53,18 +64,423 @@ public class PickUpObject : MonoBehaviour
         }
     }
 
+    //----------------------------------------------------------------------------------------------------------------------
     public void BuyItem1()
     {
-        if (wood >= 2)
+        if (wood >= 1 && stone >= 1)
         {
-            wood -= 2;
+            wood -= 1;
+            stone -= 1;
             woodCounter.text = wood.ToString();
             woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
             Building1.SetActive(true);
         }
         else
         {
             NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+    public void BuyItem2()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building2.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem3()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building3.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem4()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building4.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------\
+        public void BuyItem5()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building5.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem6()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building6.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem7()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building7.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem8()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building8.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem9()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building9.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem10()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building10.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem11()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building11.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem12()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building12.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem13()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building13.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem14()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building14.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem15()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building15.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem16()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building16.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem17()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building17.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem18()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building18.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem19()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building19.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem20()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building20.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
+        }
+    }
+    //----------------------------------------------------------------------------------------------------------------------
+        public void BuyItem21()
+    {
+        if (wood >= 1 && stone >= 1)
+        {
+            wood -= 1;
+            stone -= 1;
+            woodCounter.text = wood.ToString();
+            woodCounter1.text = wood.ToString();
+            stoneCounter.text = stone.ToString();
+            stoneCounter1.text = stone.ToString();
+            Building21.SetActive(true);
+        }
+
+        else
+        {
+            NotEnoughError.SetActive(true);
+            //StartCoroutine(Wait(2));
         }
     }
 }
