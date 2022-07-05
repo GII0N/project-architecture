a04d9534a9f15637b9555367173b0c51f43fc6e5
LegacyOfTheArdor <95230814+LegacyOfTheArdor@users.noreply.github.com>
2022-07-05 10:23:56 +0200

    TimeLimitBar fixed

--- Assets/Scripts/Menu Scripts/PauseMenu.cs
+++ Assets/Scripts/Menu Scripts/PauseMenu.cs
@@ -34,9 +34,11 @@ public class PauseMenu : MonoBehaviour
 				Pause();
 			}
 		}
-	
-		UpdateProgress();
-		
+
+		if (Time.timeScale == 1f)
+		{
+			UpdateProgress();
+		}
 	}
 	public void Resume()
 	{
